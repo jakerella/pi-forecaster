0d53fc945b273d2968fff39efc9a506047ba9e91
Jordan Kasper <jordan@jordankasper.com>
2026-02-08 13:22:04 -0500

    Added text to speech engine

--- forecaster.py
+++ forecaster.py
@@ -2,6 +2,7 @@ import time
 import board
 from digitalio import DigitalInOut, Direction, Pull
 import adafruit_dotstar
+import pyttsx3
 
 DOTSTAR_DATA = board.D5
 DOTSTAR_CLOCK = board.D6
@@ -10,6 +11,8 @@ dots = adafruit_dotstar.DotStar(DOTSTAR_CLOCK, DOTSTAR_DATA, 3, brightness=0.01)
 button = DigitalInOut(board.D17)
 button.direction = Direction.INPUT
 button.pull = Pull.UP
+
+speechEngine = pyttsx3.init()
 
 def setDots(wheel):
     for i in range(len(dots)):
@@ -21,7 +24,10 @@ def getWeather():
     time.sleep(3)
     print("  voicing forecast.")
     setDots((0, 0, 255))
-    time.sleep(3)
+    speechEngine.say('Hello! This is the current forecast.')
+    speechEngine.runAndWait()
+    speechEngine.stop()
+    time.sleep(2)
 
 setDots((0, 255, 0))
 print("Waiting for button press...\n")
