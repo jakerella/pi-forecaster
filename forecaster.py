3867be893cd64207a43b5093a1351c6a6b2c4a25
Jordan Kasper <jordan@jordankasper.com>
2026-02-08 15:29:12 -0500

    Added http request to get forecast and short vs logn button press for today vs tomorrow

--- forecaster.py
+++ forecaster.py
@@ -3,37 +3,79 @@ import board
 from digitalio import DigitalInOut, Direction, Pull
 import adafruit_dotstar
 import pyttsx3
+import os
+import time
+import requests
 
+print("\nStarting Pi-Forecaster...")
+
+print("  getting LED dots...")
 DOTSTAR_DATA = board.D5
 DOTSTAR_CLOCK = board.D6
 dots = adafruit_dotstar.DotStar(DOTSTAR_CLOCK, DOTSTAR_DATA, 3, brightness=0.01)
 
+print("  getting button...")
 button = DigitalInOut(board.D17)
 button.direction = Direction.INPUT
 button.pull = Pull.UP
 
+print("  initializing text-to-speech...")
 speechEngine = pyttsx3.init()
 
-def setDots(wheel):
+def start():
+    setDotColor((0, 255, 0))
+    print("  setup complete.\n")
+    print("Waiting for button press...\n")
+    btn_prev_state = button.value
+    btn_down_ts = 0
+    while True:
+        btn_state = button.value
+        if btn_state != btn_prev_state:
+            if not btn_state:
+                btn_down_ts = time.time()
+            else:
+                ts_diff = time.time() - btn_down_ts
+                print("  button held for " + str(ts_diff) + " seconds...")
+                day = "today"
+                if ts_diff > 1.5:
+                    day = "tomorrow"
+                getWeather(day)
+                setDotColor((0, 255, 0))
+                print("\nWaiting for button press...\n")
+        btn_prev_state = btn_state
+
+
+def setDotColor(wheel):
     for i in range(len(dots)):
         dots[i] = wheel
 
-def getWeather():
-    print("  button pressed, getting weather forecast...")
-    setDots((255, 0, 0))
-    time.sleep(3)
-    print("  voicing forecast.")
-    setDots((0, 0, 255))
-    speechEngine.say('Hello! This is the current forecast.')
+
+def getWeather(day="today"):
+    print("  retrieving weather forecast for " + day + "...")
+    setDotColor((255, 0, 0))
+
+    response = requests.get(
+        "https://jordankasper.com/.netlify/functions/getWeather?date=" + day,
+        headers = { "Authorization": os.getenv("WEATHER_KEY", None) }
+    )
+    data = response.json()
+    message = "No forecast available for " + day
+    if response.status_code == 200:
+        print(data["forecast"])
+        message = data["forecast"]
+    else:
+        print(data["message"])
+        message = data["message"]
+
+    print("  voicing forecast...")
+    setDotColor((0, 0, 255))
+    speechEngine.say(message)
     speechEngine.runAndWait()
     speechEngine.stop()
-    time.sleep(2)
+    print("  complete.")
+    time.sleep(1)
 
-setDots((0, 255, 0))
-print("Waiting for button press...\n")
-while True:
-    if not button.value:
-        getWeather()
-        setDots((0, 255, 0))
-        print("Waiting for button press...")
-    time.sleep(0.2)
+
+#######
+start()
+#######
